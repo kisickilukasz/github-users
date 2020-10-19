44565213fe95d154d20a166ae837a45fd680f5ca
Łukasz Kisicki <lukasz.kisicki@getresponse.com>
2020-10-19 15:11:01 +0200

    test: testing setup

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React the hard way!!
+          Learn React the hard way!
+          Also checking repo setup.
         </a>
       </header>
     </div>
